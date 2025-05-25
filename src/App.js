a7816da96682e7d15ba063d9bbe4ea79410a8c7c
Sol <sol@Ss-MacBook-Pro.local>
2025-05-25 15:05:31 +0100

    Remove all white space - update App.js and index.css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import LocalAnaestheticCalculator from './components/LocalAnaestheticCalculator'
 
 function App() {
   return (
-    <div style={{
-      backgroundColor: '#f3f4f6',
-      minHeight: '100vh',
-      padding: '16px'
-    }}>
-      <LocalAnaestheticCalculator />
-    </div>
+    <LocalAnaestheticCalculator />
   );
 }
 
